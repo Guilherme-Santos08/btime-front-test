68a8e870d9f4031006c3d7cee29842548159f77f
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-08-24 09:37:08 -0300

    zerando css dos navegadores

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
-
+  width: 230px;
+  height: 260px;
+  border-radius: 8px;
+  border: 1px solid red;
 `
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    scroll-behavior: smooth;
+  }
+  body {
+    color: white;
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+    padding: 0;
+  }
+`;
